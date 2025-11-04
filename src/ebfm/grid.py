3b52e7579c9d60a7e2ee0e62e3340c646a51447c
Benjamin Rodenberg <rodenberg@dkrz.de>
2025-11-04 14:09:03 +0100

    Add clarifications.

--- src/ebfm/grid.py
+++ src/ebfm/grid.py
@@ -1,6 +1,6 @@
 from enum import Enum
 
 class GridInputType(Enum):
-    MATLAB = 'matlab'
-    CUSTOM = 'custom'
-    ELMER = 'elmer'
+    MATLAB = 'matlab'  # .mat grid file with elevation
+    CUSTOM = 'custom'  # Elmer/Ice mesh file for xy-coordinates and separate NetCDF elevation file
+    ELMER = 'elmer'    # Elmer/Ice mesh file with elevation in z-coordinate
